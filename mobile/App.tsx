c0e4b9a8ab3a53d8bc6833bc658db3cd2754bacc
Giovanna Moeller <giovannamoeller@MacBook-Air-de-Giovanna.local>
2020-08-02 23:48:09 -0300

    Finalizando página inicial

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,24 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, View } from 'react-native';
+import { AppLoading } from 'expo';
+import Home from './src/pages/Home';
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
 
 // JSX - XML dentro do JS
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading/>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Deu ssssss</Text>
-      <Text>Olá</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Home/>
+      <StatusBar barStyle="dark-content" translucent/>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
